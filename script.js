a088ecdbece354cf47b62833a95316aba010e193
praptibargir <praptibargir21@gmail.com>
2024-04-27 14:07:53 +0530

    assignment 35 completed

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const todoList = [];
+let todoList = [];
+
+const listFromLocalStorage =localStorage.getItem("todoList");
+if(listFromLocalStorage){
+    todoList=JSON.parse(listFromLocalStorage);
+    renderToDoList();
+}
 
 function add(){
     const inputElement = document.getElementById('input-todo');
     todoList.push(inputElement.value);
+    localStorage.setItem("todoList",JSON.stringify(todoList));
     inputElement.value = "";
 
     renderToDoList();
